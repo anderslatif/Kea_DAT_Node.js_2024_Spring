05da14ca58845d19a5b1ddb962f21deca43fed3c
anderslatif <anderslatif@users.noreply.github.com>
2024-02-02 10:50:49 +0100

    Added the rest of the intro files

---
+++ 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/3.Objects.js
@@ -0,0 +1,33 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObject = { message: "Hello, earthling! I bring peace." };
+
+// Log the message 
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+// Create an object that has your name and age. 
+
+
+// --------------------------------------
+// Exercise 3 - Add a key-value pair 
+
+const stackOverflow = {};
+
+// make a rule called isAllowed and let the value be true
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = { description: "The best song in the world." };
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+
+// --------------------------------------
+
+
---
+++ 00._Course_Material/01._Assignments/01._Introduction/01._Introduction_Exercises/4.Arrays.js
@@ -0,0 +1,75 @@
+// --------------------------------------
+// Arrays, for loops
+// --------------------------------------
+// Exercise 1 - Array Positioning
+
+const letters = ["a", "b", "c"];
+// show b in the console 
+
+
+// --------------------------------------
+// Exercise 2 - Array Positioning
+
+const friends = [];
+
+// What a lonely array. Add at least 3 friend objects to it.  
+
+
+// --------------------------------------
+// Exercise 3 - Get the index of first occurance of that value. 
+
+const significantMathNumbers = [0, 2.718, 3.14159, 1729];
+
+// You want to programmatically find where the number 1729 is in the array.
+// programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
+
+
+// --------------------------------------
+// Exercise 4 - Inserting elements
+
+const diet = ["tomato", "cucumber", "rocket"];
+
+// You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
+
+
+
+
+// --------------------------------------
+// Exercise 5 - Remove element
+
+// Remove the LAST element of the array.
+// Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
+
+
+
+
+
+// --------------------------------------
+// Exercise 6 - Copy array
+
+// You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
+
+
+
+// --------------------------------------
+// Exercise 7 - For loop
+
+const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+// log every second char in the array starting from b
+
+
+
+// --------------------------------------
+// Exercise 8 - For loop and if statement
+
+const numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
+
+const discardedNumbers = [];
+
+// log the element if the number is above 6 or below 0
+// else push them to the array discardedNumbers
+
+// --------------------------------------
+
+
