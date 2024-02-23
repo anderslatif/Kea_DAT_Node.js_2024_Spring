fd0cdbdecef150f6987b06e30cb2852bbbfa31ff
anderslatif <anderslatif@users.noreply.github.com>
2024-02-23 09:19:36 +0100

    Finished the Drinks API

--- 03._Drinks_API/app.js
+++ 03._Drinks_API/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const drinks = [
     { id: 1, name: "Mojito" },
     { id: 2, name: "Isbjørn" },
     { id: 3, name: "Old Fashion" },
 ];
+
+let nextId = 4;
 
 app.get("/drinks", (req, res) => {
     res.send({ data: drinks });
@@ -16,9 +20,40 @@ app.get("/drinks/:id", (req, res) => {
     const providedDrinkId = Number(req.params.id);
     const foundDrink = drinks.find((drink) => drink.id === providedDrinkId);
     if (!foundDrink) {
-        res.status(404).send({ data: "Drink not found" });
+        res.status(404).send({ error: `Drink not found by id: ${providedDrinkId}.` });
     } else {
         res.send({ data: foundDrink });
+    }
+});
+
+app.post("/drinks", (req, res) => {
+    const newDrink = req.body;
+    newDrink.id = nextId++;
+    drinks.push(newDrink);
+    res.send({ data: newDrink });
+});
+
+app.patch("/drinks/:id", (req, res) => {
+    const providedDrinkId = Number(req.params.id);
+    const foundDrinkIndex = drinks.findIndex(drink => drink.id === providedDrinkId);
+    if (foundDrinkIndex === -1) {
+        res.status(404).send({ error: `Drink not found by id: ${providedDrinkId}.` });
+    } else {
+        const originalDrink = drinks[foundDrinkIndex];
+        const drinkToUpdate = { ...originalDrink, ...req.body, id: providedDrinkId };
+        drinks[foundDrinkIndex] = drinkToUpdate;
+        res.send({ data: drinkToUpdate });
+    }
+});
+
+app.delete("/drinks/:id", (req, res) => {
+    const providedDrinkId = Number(req.params.id);
+    const foundDrinkIndex = drinks.findIndex((drink) => drink.id === providedDrinkId);
+    if (foundDrinkIndex === -1) {
+        res.status(404).send({ error: `Drink not found by id: ${providedDrinkId}.` });
+    } else {
+        drinks.splice(foundDrinkIndex, 1);
+        res.send({ data: providedDrinkId });
     }
 });
 
