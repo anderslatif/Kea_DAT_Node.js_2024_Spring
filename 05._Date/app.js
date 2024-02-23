6c5478b7e85f10165f0f13d9bcc08ff3772c59a1
anderslatif <anderslatif@users.noreply.github.com>
2024-02-23 10:14:23 +0100

    Finished working with Date

--- 05._Date/app.js
+++ 05._Date/app.js
@@ -11,10 +11,36 @@ console.log(Date.now());
 // Local time (in my case right now and here CEST GMT+0100)
 console.log(Date());
 
-// assignment       create a route with the endpoint /month 
-// assignment       that returns the current month
 
+app.get("/date", (req, res) => {
+    res.send({ data: Date() });
+});
 
+const months = ["January", "February", "March", "April", "May", "June", "July", 
+                "August", "September", "October", "November", "December"];
+
+app.get("/month/version1", (req, res) => {
+    const monthIndex = new Date().getMonth();
+
+    res.send({ data: months[monthIndex] });
+});
+
+app.get("/month/version2", (req, res) => {
+    const monthName = new Date().toLocaleDateString("en-us", { month: "long" })
+    res.send({ data: monthName });
+});
+
+app.get("/month/version3", (req, res) => {
+    const monthName = Date().split(" ")[1];
+    res.send({ data: monthName });
+});
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+app.get("/day", (req, res) => {
+    const dayName = days[new Date().getDay()];
+    res.send({ data: dayName });
+});
 
 const PORT = 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
