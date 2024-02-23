9ee98e346da943f21a6c368914a503843db2a172
anderslatif <anderslatif@users.noreply.github.com>
2024-02-23 11:28:25 +0100

    Implemented the greeting feature

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -7,11 +7,20 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/homepage/homepage.html");
 });
 
-
-
 app.get("/publicsquare", (req, res) => {
     res.sendFile(__dirname + "/public/publicSquare/publicSquare.html");
 });
+
+const knownName = "Anders";
+
+app.get("/greeting", (req, res) => {
+    if (req.query.name === knownName) {
+        res.send({ data: `Hello ${knownName}!` });
+    } else {
+        res.send({ data: "Hello stranger!" });
+    }
+});
+
 
 const PORT = 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
