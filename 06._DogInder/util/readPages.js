dd798291354a86ce715e164633848ae41958e021
anderslatif <anderslatif@users.noreply.github.com>
2024-03-08 11:45:39 +0100

    Finished refactoring to SSR

--- 06._DogInder/util/readPages.js
+++ 06._DogInder/util/readPages.js
@@ -4,8 +4,13 @@ const homepage = readPage("./public/pages/homepage/homepage.html");
 export const homepagePage = renderPage(homepage);
 
 const matches = readPage("./public/pages/matches/matches.html");
-export const matchesPage = renderPage(matches);
+export const matchesPage = renderPage(matches, {
+    tabTitle: "DogInder Match",
+    CSSLinks: '<link rel="stylesheet" href="/pages/matches/matches.css">'
+});
 
 const contact = readPage("./public/pages/contact/contact.html");
-export const contactPage = renderPage(contact);
+export const contactPage = renderPage(contact, {
+    tabTitle: "DogInder Contact"
+});
 
--- 06._DogInder/util/templatingEngine.js
+++ 06._DogInder/util/templatingEngine.js
@@ -4,9 +4,13 @@ export function readPage(path) {
     return fs.readFileSync(path).toString();
 }
 
-const header = fs.readFileSync("./public/components/header/header.html").toString();
 const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
 
-export function renderPage(page) {
-    return header + page + footer;
+export function renderPage(page, config={}) {
+    const header = fs.readFileSync("./public/components/header/header.html").toString();
+    return header.replace("$TAB_TITLE$", config.tabTitle || "DogInder")
+                .replace("$CSS_LINKS$", config.CSSLinks || "")
+     + page 
+     + footer;
 }
+
