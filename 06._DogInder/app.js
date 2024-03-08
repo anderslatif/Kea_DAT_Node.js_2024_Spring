840d7c25a3b9b1e5f791d94b60610b09e97a1bae
anderslatif <anderslatif@users.noreply.github.com>
2024-03-08 10:56:06 +0100

    Started converting to SSR

--- 06._DogInder/app.js
+++ 06._DogInder/app.js
@@ -8,18 +8,37 @@ import path from "path";
 
 import getMatches from "./util/matches.js";
 
+import fs from "fs";
+
+const header = fs.readFileSync("./public/components/header/header.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+
+const homepage = fs.readFileSync("./public/pages/homepage/homepage.html").toString();
+const matches = fs.readFileSync("./public/pages/matches/matches.html").toString();
+const contact = fs.readFileSync("./public/pages/contact/contact.html").toString();
+
+const homepagePage = header + homepage + footer;
+const matchesPage = header + matches + footer;
+const contactPage = header + contact + footer;
+
+
 // ========= HTML =================
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/pages/homepage/homepage.html"));
+    res.send(homepagePage);
 });
 
 app.get("/matches", (req, res) => {
-    res.sendFile(path.resolve("public/pages/matches/matches.html"));
+    res.send(matchesPage);
 });
 
 app.get("/contact", (req, res) => {
-    res.sendFile(path.resolve("public/pages/contact/contact.html"));
+    res.send(contactPage);
+});
+
+
+app.get("/page", (req, res) => {
+    res.send("<div><h1>sdfdsfdsf</h1><h2>sdfdsfdsfdsfsdfsdf</h2></div>");
 });
 
 // ========== API =================
